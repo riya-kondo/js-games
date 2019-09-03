289a1dec496f57708f202a6bb486ab2cb311cba8
riya-kondo <hosa.ashes@gmail.com>
2019-09-03 14:12:35 +0900

    bar class is added. 4 bars is randomly created when game is loaded

--- hopping/hopping.js
+++ hopping/hopping.js
@@ -2,10 +2,16 @@ const width = 380;
 const height = 680;
 const size = 40;
 const gravity = 1;
+var bars = new Array();
 
 function setup(){
   createCanvas(width, height);
   hopper = new Hopper();
+  let range = height/4;
+  for(let i=0; i<4; i++){
+    bar = new Bar(height-(range+range*i));
+    bars.push(bar);
+  }
 }
 
 function draw(){
@@ -15,6 +21,9 @@ function draw(){
     hopper.move(3); 
   } else if (keyIsDown(LEFT_ARROW)) {
     hopper.move(-3);
+  }
+  for(let i=0; i<bars.length; i++){
+    bars[i].update();
   }
   hopper.switchSpeed();
   hopper.update();
@@ -48,5 +57,21 @@ class Hopper{
     }else if(this.x+this.size/2 < 0){
       this.x = width;
     }
+  }
+}
+
+class Bar{
+  constructor(y=0){
+    this.width = random(size*2, size*4);
+    this.x = random(0, width-this.width);
+    this.y = y;
+    this.yspeed = 0;
+    this.height = 20;
+  }
+
+  update(){
+    fill('#000');
+    this.y += this.yspeed;
+    rect(this.x, this.y, this.width, this.height, 10, 10, 10, 10)
   }
 }
